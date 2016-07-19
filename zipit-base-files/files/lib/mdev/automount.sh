8463cb07e7f08e1b288f0f8c74f8983d324d3e3a
Joe Honold <mozzwald@gmail.com>
2016-07-19 10:09:27 -0500

    zipit-base-files: automount: don't mount rootfs to /mnt
    Signed-off-by: Joe Honold <mozzwald@gmail.com>

--- zipit-base-files/files/lib/mdev/automount.sh
+++ zipit-base-files/files/lib/mdev/automount.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# mdev automount script
+# mdev automount filesystems
+# http://www.armadeus.org/wiki/index.php?title=Automatically_mount_removable_media
 
 destdir=/mnt
 
@@ -14,12 +15,16 @@ my_umount()
 
 my_mount()
 {
-	mkdir -p "${destdir}/$1" || exit 1
+	# skip mounting if device is rootfs
+	/bin/grep -q "root=/dev/$1" /proc/cmdline
+	if [ $? -ne 0 ]; then
+		mkdir -p "${destdir}/$1" || exit 1
 
-	if ! mount -t auto -o sync "/dev/$1" "${destdir}/$1"; then
-		# failed to mount, clean up mountpoint
-		rmdir "${destdir}/$1"
-		exit 1
+		if ! mount -t auto -o async "/dev/$1" "${destdir}/$1"; then
+			# failed to mount, clean up mountpoint
+			rmdir "${destdir}/$1"
+			exit 1
+		fi
 	fi
 }
 
