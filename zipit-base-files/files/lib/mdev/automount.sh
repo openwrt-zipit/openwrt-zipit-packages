69e8977c9877862e235dae7d89ab5188114ed0ba
Joe Honold <mozzwald@gmail.com>
2016-09-12 20:19:16 -0500

    zipit-base-files: automount, profile and init script updates Update automount script to support mounting by disk label if blkid package is installed. Update profile to export TERM=linux Add z2gpio init script to export dock gpios in sysfs
    Signed-off-by: Joe Honold <mozzwald@gmail.com>

--- zipit-base-files/files/lib/mdev/automount.sh
+++ zipit-base-files/files/lib/mdev/automount.sh
@@ -4,13 +4,31 @@
 
 destdir=/mnt
 
+# Mount by filesystem label; requires blkid package
+if [ -x /usr/sbin/blkid ]; then
+	label=`/usr/sbin/blkid "/dev/${MDEV}" | sed 's/.*LABEL="//' | sed 's/".*//'`
+	# if disk has no label, mount by device
+	if [ -z "$label" ]; then
+		bylabel=0
+	else
+		bylabel=1
+		# check for duplicate label; add part of UUID if duplicate
+		if grep -qs "${label}" /proc/mounts ; then
+			label="${label}_`/usr/sbin/blkid "/dev/${MDEV}" | sed 's/.*PARTUUID="//' | sed 's/".*//'`"
+		fi
+	fi
+else
+	bylabel=0
+fi
+
 my_umount()
 {
 	if grep -qs "^/dev/$1 " /proc/mounts ; then
-		umount "${destdir}/$1";
+		umount "/dev/$1";
 	fi
 
 	[ -d "${destdir}/$1" ] && rmdir "${destdir}/$1"
+	[ -d "${destdir}/${label}" ] && rmdir "${destdir}/${label}"
 }
 
 my_mount()
@@ -18,12 +36,22 @@ my_mount()
 	# skip mounting if device is rootfs
 	/bin/grep -q "root=/dev/$1" /proc/cmdline
 	if [ $? -ne 0 ]; then
-		mkdir -p "${destdir}/$1" || exit 1
-
-		if ! mount -t auto -o async "/dev/$1" "${destdir}/$1"; then
-			# failed to mount, clean up mountpoint
-			rmdir "${destdir}/$1"
-			exit 1
+		if [ $bylabel -eq 1 ]; then
+			# Mount by label
+			mkdir -p "${destdir}/${label}" || exit 1
+			if ! mount -t auto -o async "/dev/$1" "${destdir}/${label}"; then
+				# failed to mount, clean up mountpoint
+				rmdir "${destdir}/${label}"
+				exit 1
+			fi
+		else
+			# Mount by device id
+			mkdir -p "${destdir}/$1" || exit 1
+			if ! mount -t auto -o async "/dev/$1" "${destdir}/$1"; then
+				# failed to mount, clean up mountpoint
+				rmdir "${destdir}/$1"
+				exit 1
+			fi
 		fi
 	fi
 }
