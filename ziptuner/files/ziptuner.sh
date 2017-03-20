dcb8be49e331979d7d09ec501b0bd533c71b8957
Joe Honold <mozzwald@gmail.com>
2017-03-19 19:06:45 -0500

    ziptuner: fix path to ziptuner binary in shell script

--- ziptuner/files/ziptuner.sh
+++ ziptuner/files/ziptuner.sh
@@ -2,7 +2,5 @@
 
 cd /usr/share/ziptuner
 
-#TERM=xterm-color DIALOGRC=/usr/share/ziptuner/dialogrc.soho /usr/share/ziptuner.bin -mp3 "mpg123tty4 -@ " -p "mplayertty4 -playlist " -s "killall mpg123; killall mplayer" /root/radio /root/.config/gmu/playlist.m3u
-
-TERM=xterm-color DIALOGRC=/usr/share/ziptuner/dialogrc.soho /usr/share/ziptuner.bin -mp3 "mpg123tty4 -@ " -p "mplayertty4 -playlist " -s "killall mpg123; killall mplayer" /root/radio /root/.local/share/gmu/playlist.m3u
+TERM=xterm-color DIALOGRC=/usr/share/ziptuner/dialogrc.soho /usr/bin/ziptuner.bin -mp3 "mpg123tty4 -@ " -p "mplayertty4 -playlist " -s "killall mpg123; killall mplayer" /root/radio /root/.local/share/gmu/playlist.m3u
 
