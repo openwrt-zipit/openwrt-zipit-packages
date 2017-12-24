294f18714e213834b9506dc299a31af01031c0c7
deeice <dheyse@hotmail.com>
2017-12-24 17:41:04 -0500

    Added raw option sendtty to allow for shifted or modified keys.  Used that for shift-esc to end gmu from onStop.

--- ebindkeys-zipit/files/sendtty.c
+++ ebindkeys-zipit/files/sendtty.c
@@ -17,9 +17,11 @@ int main(int argc, char **argv)
     if ((hTTY = open(argv[1], O_WRONLY|O_NONBLOCK)) < 0)
       return hTTY;
     while (c[0] = *(argv[2]++)) { // Send a string to the tty.
-      ioctl(hTTY, TIOCSTI, c);
-      c[0] = c[0] | 0x80; // press and release
-      ioctl(hTTY, TIOCSTI, c);
+      ioctl(hTTY, TIOCSTI, c); // send a keypress
+      if (argc == 3) { // If extra arg use "raw" mode (no auto key release)
+          c[0] = c[0] | 0x80; // send (auto) release
+          ioctl(hTTY, TIOCSTI, c);
+      }
     }
     close(hTTY);
   }
